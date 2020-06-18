aa02a263a04ca989e15f7a8c927c19ad3bd4687b
Cheese229 <63216824+Cheese229@users.noreply.github.com>
2020-06-18 21:04:54 +1000

    assignment and experiments/sides
    stuff

---
+++ PythonAssignment/experiment.py
@@ -0,0 +1,11 @@
+import numpy as np
+import time 
+import matplotlib.pyplot as plt
+import statistics
+import simpy
+import random
+
+n_samples = 10
+n_structure = 50 + 50 * np.random.rand(n_samples, 2), None
+X, y = n_structure
+
